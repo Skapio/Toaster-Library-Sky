10d451f0ff58db6782eb4afaeacb73bb0d36156a
Pasiut_Artur <pasiut1998@gmail.com>
2021-09-21 15:02:32 +0200

    some changes

--- ToasterLibrarySky/src/main/java/com/example/toasterlibrarysky/ToasterMessage.kt
+++ ToasterLibrarySky/src/main/java/com/example/toasterlibrarysky/ToasterMessage.kt
@@ -5,7 +5,7 @@ import android.widget.Toast
 
 class ToasterMessage {
 
-    fun show(context: Context, message: String) {
+    fun showItIsMyOwnFunction(context: Context, message: String) {
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/example/toasterexample/MainActivity.kt
+++ app/src/main/java/com/example/toasterexample/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.toasterexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.toasterlibrarysky.ToasterMessage
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
